d7cc94d38ea9f08853dfb7cc216c2181dccfdd62
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-11-12 23:44:03 -0500

    Add initial structure for Lab10.py with functions
    lab file create commit

---
+++ Lab10.py
@@ -0,0 +1,24 @@
+
+##  **Lab10.py**
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Function 1: Mathematical Curve using np.linspace()
+def plot_curve():
+    return
+
+# Function 2: Sine and Cosine using np.arange()
+def plot_trig_functions():
+    return
+
+# Function 3: Temperature Data from .txt using Pandas
+def plot_temperature_data(filename):
+    return
+
+# Function 4: Population Data from .csv using Pandas
+def plot_population_data(filename):
+    return
+
+---
